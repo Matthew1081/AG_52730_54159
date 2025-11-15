f2dab37dbb02d2134b974864dbdd7730d03d60dc
Matthew1081 <m.kwiatkowski1078@gmail.com>
2025-11-15 10:18:25 +0100

    import +load data
    add import + load data to files

--- import_data.py
+++ import_data.py
@@ -1,6 +1,6 @@
-def data(filename):
+def data_dynamic(filename):
     items = []
-    sorce  = open(filename, "r")
+    sorce = open(filename, "r")
     first_line = sorce.readline().strip()
     split_line = first_line.split()
     n = int(split_line[0])
@@ -15,5 +15,30 @@ def data(filename):
     
     sorce.close()
     return n, capacity, items
+
+
+def dynamic_backpack(n, capacity, items):
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        weight, value = items[i - 1]
+        for w in range(capacity + 1):
+            if weight <= w:
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weight] + value)
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    chosen_items = []
+    w = capacity
+    for i in range(n, 0, -1):
+        if dp[i][w] != dp[i - 1][w]:
+            chosen_items.append(i - 1)
+            w -= items[i - 1][0]
+    chosen_items.reverse()
+    
+    
+    return dp[n][capacity], chosen_items
+
+   
 
 
--- load_data.py
+++ load_data.py
@@ -1,8 +1,23 @@
-from import_data import data
+from import_data import data_dynamic, dynamic_backpack
 
 sciezka = r"C:\Users\mkwia\Desktop\dsw\algorytmy genetyczne i sztuczne sieci neuronowe\AG_52730_54159\large_scale\knapPI_2_200_1000_1.txt"
-n, capacity, items = data(sciezka)
+n, capacity, items = data_dynamic(sciezka)
 
 print("Liczba przedmiotów:", n)
 print("Pojemność plecaka:", capacity)
 print("Pierwsze 5 przedmiotów:", items[:5])
+
+max_value, chosen_items = dynamic_backpack(n, capacity, items)
+print("Maksymalna wartość w plecaku:", max_value)
+print("Wybrane przedmioty (indeksy):", chosen_items)
+print("Wybrane przedmioty (waga, wartość):", [items[i] for i in chosen_items])
+sum_value = 0
+sum_weight = 0
+
+for i in chosen_items: 
+    veight, value = items[i]
+    sum_value += value
+    sum_weight += veight
+print("Suma wag wybranych przedmiotów:", sum_weight)
+print("Suma wartości wybranych przedmiotów:", sum_value)
+print("Sprawdzenie poprawności sumy wartości:", "Poprawne" if sum_value == max_value else "Błąd")
